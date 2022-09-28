5733e94d6504a3afdbc9d242b8dfd9e70f912c34
gabang2 <offbeat1020@naver.com>
2022-09-29 00:34:39 +0900

    Tutorial(Requests and Responses)

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail)
 ]
+
+# url 뒤에 .json등 확장자 정보를 붙여 확장자를 받아올 수 있음
+urlpatterns = format_suffix_patterns(urlpatterns)
