05837b30e7d33f50d8427017fda6999ff7f7f0c3
gabang2 <offbeat1020@naver.com>
2022-10-03 19:55:32 +0900

    Tutorial(Class Based View) - 함수형을 클래스로 변환함

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
+from snippets.views import SnippetList, SnippetDetail
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail)
+    path('snippets/', SnippetList.as_view()),
+    path('snippets/<int:pk>/', SnippetDetail.as_view())
 ]
 
 # url 뒤에 .json등 확장자 정보를 붙여 확장자를 받아올 수 있음
